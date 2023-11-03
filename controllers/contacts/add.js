87d6d71b8c677d390bfeffb6140958da64cca0f2
OlekSandr-offensive <oleksandr.tarasyuk92@gmail.com>
2023-11-03 22:08:52 +0100

    fix add

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,10 +1,10 @@
 const { Contact } = require("../../models/contact");
 
 const add = async (req, res) => {
-  const { _id: owner } = req.user;
+  // const { _id: owner } = req.user;
   const body = req.body;
 
-  const result = await Contact.create({ ...body, owner });
+  const result = await Contact.create({ ...body });
   res.status(201).json(result);
 };
 
