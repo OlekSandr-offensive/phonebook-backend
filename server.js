e3fbbcf2413ddbc3fb8c2d3fd06f63bd36c2e423
OlekSandr-offensive <oleksandr.tarasyuk92@gmail.com>
2023-11-03 18:37:47 +0100

    clear

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
 
 const { MONGO_URL, PORT = 3000 } = process.env;
 
-mongoose.set("useFindAndModify", false);
+mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URL)
   .then(() => {
     app.listen(PORT, () => {
       console.log("Database connect successfully");
