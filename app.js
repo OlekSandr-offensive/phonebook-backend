fe92e45bf94a7fb505466eefe5501bb1a50fa2b7
OlekSandr-offensive <oleksandr.tarasyuk92@gmail.com>
2024-07-13 09:46:24 +0200

    delete change

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use("/api/auth", authRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
-  res.header("Access-Control-Allow-Origin", "*");
 });
 
 app.use((err, _, res, __) => {
