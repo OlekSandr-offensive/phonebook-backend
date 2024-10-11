c32a2fb2b816d49981fd3a4ba557f86cb6f14ee5
OlekSandr-offensive <oleksandr.tarasyuk92@gmail.com>
2024-10-11 20:39:47 +0200

    new port

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 const { MONGO_URL } = process.env;
+const port = process.env.PORT || 4000;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(MONGO_URL)
   .then(() => {
-    app.listen(4000, () => {
+    app.listen(port, () => {
       console.log("Database connect successfully");
     });
   })
